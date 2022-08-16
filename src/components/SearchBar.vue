cb453b5e4067a953f57aeb18fe52d7b9e87c9c58
korbal <balint.j.korosi@gmail.com>
2022-08-16 18:33:51 +0200

    input filter works

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,13 @@
  <template>
    <input type="text" v-model="input" placeholder="Search fruits..." />
-  <div class="item fruit" v-for="fruit in filteredList()" :key="fruit">
+  <div class="item fruit" v-for="fruit in filteredList()" :key="fruit" v-show="input.length !=0">
     <p>{{ fruit }}</p>
   </div>
   <div class="item error" v-if="input&&!filteredList().length">
      <p>No results found!</p>
   </div>
 </template>
+
 
 
 <script setup>
