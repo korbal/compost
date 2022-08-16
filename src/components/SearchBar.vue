071ce8741ee256d9a769d2f0fc51357cd7428f91
korbal <balint.j.korosi@gmail.com>
2022-08-16 19:56:50 +0200

    'deploy'

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -36,7 +36,7 @@ input {
   background-size: 15px 15px;
   font-size: 16px;
   border: none;
-  border-radius: 5px;
+  border-radius: 20px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 }
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-// vue.config.js file to be place in the root of your repository. used for local dev. or maybe not? 
+// vue.config.js file to be place in the root of your repository.used for local dev.or maybe not ?
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/compost/'
-//     : '/'
-// }
-
-
-//to deploy on github pages I need to use this otherwise 404
 module.exports = {
-  publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/compost/'
+    : '/'
 }
+
+
+// //to deploy on github pages I need to use this otherwise 404
+// module.exports = {
+//   publicPath: '/'
+// }
