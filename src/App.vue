1ecb79e447334ccc9803d7fd18825d617b5ef4e1
korbal <balint.j.korosi@gmail.com>
2022-08-16 17:48:33 +0200

    'deploy'

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,22 @@
 
 <style>
 
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+
+body {
+  padding: 20px;
+  min-height: 100vh;
+  background-color: rgb(234, 242, 255);
+  background-image: url('./assets/carrots_small.jpg');
+  background-size: cover;
+  max-height: 100vh;
+  height: 100vh;
+}
 
 
 </style>
