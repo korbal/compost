0c7e8e7aeee5747c3149d7a905b6f29a27d1986f
korbal <balint.j.korosi@gmail.com>
2022-08-18 17:59:30 +0200

    'deploy'

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from './router'
 //////////////////////////////////////////////////////////////////////
 
 // enable this if you want to use google analytics
-// import VueGtag from "vue-gtag";
+import VueGtag from "vue-gtag";
 createApp(App).use(VueGtag, { config: { id: "G-LX4H5N8Z0Y" } }).use(VueGtag, { config: { id: "UA-3450053-63" } }).use(router).mount("#app");
 
 // disable this if you want to use google analytics
