48e604c73d6ae33a799079f494472330546fedc2
korbal <balint.j.korosi@gmail.com>
2022-08-18 15:15:31 +0200

    changed vue.config and deploying with custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-// vue.config.js file to be place in the root of your repository.used for local dev.or maybe not ?
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/compost/'
-    : '/'
-}
-
-
-// //to deploy on github pages I need to use this otherwise 404
+// vue.config.js file to be place in the root of your repository.used for local dev IF there is no custom domain associated with it on Github pages
+// once there is, comment out the following line and uncomment the next one
 // module.exports = {
-//   publicPath: '/'
-// }
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/compost/'
+//     : '/'
+//}
+
+
+//to deploy on github pages I need to use this otherwise 404
+module.exports = {
+  publicPath: '/'
+}
