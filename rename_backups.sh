a942d12e04a62e1b5425ba28e4857bcca4ce1e53
korbal <balint.j.korosi@gmail.com>
2022-08-21 14:57:13 +0200

    'deploy'

--- rename_backups.sh
+++ rename_backups.sh
@@ -1,4 +1,8 @@
-# Navigating back to src to set dev config back
+#!/usr/bin/env sh
 
+# Setting Development config live after deploy
+mv main_backup.js main.js
 
+# Recreating original state for main_deploy file backup.
+mv main_deploy_backup.js main_deploy.js
 $SHELL
