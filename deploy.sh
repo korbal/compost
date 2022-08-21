0786b9a9b8482ab3adadde94a240caf3d7374345
korbal <balint.j.korosi@gmail.com>
2022-08-21 14:29:37 +0200

    'deploy'

--- deploy.sh
+++ deploy.sh
@@ -61,7 +61,7 @@ cd src
 # Setting Development config live after deploy
 mv main_backup.js main.js
 
-# Recreating original state for main_deploy file backup
+# Recreating original state for main_deploy file backup.
 mv main_deploy_backup.js main_deploy.js
 
 echo 'deploy succesful'
