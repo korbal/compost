73af4bb018830d278adf6a3d2808bbf6f709019b
korbal <balint.j.korosi@gmail.com>
2022-08-21 14:38:03 +0200

    'deploy'

--- deploy.sh
+++ deploy.sh
@@ -55,8 +55,7 @@ git push -f git@github.com:korbal/compost.git main:gh-pages
 ##################
 
 # Navigating back to src to set dev config back
-
-cd src
+cd ../src
 
 # Setting Development config live after deploy
 mv main_backup.js main.js
