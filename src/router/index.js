a67c667a3779cffbb584b42b66b09e6fa233cdce
korbal <balint.j.korosi@gmail.com>
2022-09-07 08:22:45 +0200

    revert

--- src/router/index.js
+++ src/router/index.js
@@ -54,14 +54,10 @@ const routes = [
 
 
 
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes
-// })
-
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
 
 export default router
